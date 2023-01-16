e47baccca72467402b9c2aafd509a1682fa217ab
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-16 13:53:37 -0600

    Changed static generated Step elements by an steps array, also added state to show the current active step

--- src/components/sidevar/Sidevar.js
+++ src/components/sidevar/Sidevar.js
@@ -2,12 +2,69 @@ import Step from "./Step";
 import shape1 from "../../img/shape1.png";
 import shape2 from "../../img/shape2.png";
 import shape3 from "../../img/shape3.png";
+import { useEffect, useState } from "react";
+
 export default function Sidevar() {
+  // Approach 2 dynamic step number
+  const [stepStatuses, setStepStatuses] = useState([]);
+
+  const steps = [
+    { step_number: 1, step_name: "YOUR INFO", is_active: true },
+    { step_number: 2, step_name: "SELECT PLAN", is_active: false },
+    { step_number: 3, step_name: "ADD-ONS", is_active: false },
+    { step_number: 4, step_name: "SUMMARY", is_active: false },
+  ];
+  const stepStatusHelper = [];
+  useEffect(() => {
+    // Loads initial state for every step element.
+    steps.map((step) => {
+      stepStatusHelper.push(step.is_active);
+    });
+    setStepStatuses(stepStatusHelper);
+  }, []);
+
+  // Approach 1 static step number
+  // const [step1, setStep1] = useState({
+  //   step_number: 1,
+  //   step_name: "YOUR INFO",
+  //   is_active: true,
+  // });
+  // const [step2, setStep2] = useState({
+  //   step_number: 2,
+  //   step_name: "SELECT PLAN",
+  //   is_active: false,
+  // });
+  // const [step3, setStep3] = useState({
+  //   step_number: 3,
+  //   step_name: "ADD-ONS",
+  //   is_active: false,
+  // });
+  // const [step4, setStep4] = useState({
+  //   step_number: 4,
+  //   step_name: "SUMMARY",
+  //   is_active: false,
+  // });
   return (
     <aside
       style={{ width: "275px" }}
       className="bg-purplish_blue rounded-xl overflow-hidden relative"
     >
+      <div style={{ marginBottom: "250px" }} className="pl-7 pt-8">
+        {/* Still don't know why but semicolons at the end of jsx elements create
+        vertical spaces.*/}
+        <Step
+          step_number={1}
+          step_name={"YOUR INFO"}
+          active={stepStatuses[0]}
+        />
+        <Step
+          step_number={2}
+          step_name={"SELECT PLAN"}
+          active={stepStatuses[1]}
+        />
+        <Step step_number={3} step_name={"ADD-ONS"} active={stepStatuses[2]} />
+        <Step step_number={4} step_name={"SUMMARY"} active={stepStatuses[3]} />
+      </div>
       <img
         src={shape1}
         style={{
@@ -16,7 +73,7 @@ export default function Sidevar() {
           bottom: "-31%",
           right: "-37%",
           rotate: "-45deg",
-          zIndex: "100",
+          zIndex: 1,
         }}
         alt=""
       />
@@ -29,7 +86,6 @@ export default function Sidevar() {
           bottom: "-15%",
           left: "-51%",
           rotate: "47deg",
-          zIndex: "1",
         }}
         alt=""
       />
@@ -41,30 +97,12 @@ export default function Sidevar() {
           position: "absolute",
           bottom: "21%",
           left: "55%",
-          zIndex: "9999",
+          zIndex: 2,
         }}
         alt=""
       />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </aside>
   );
-  <Step step_number={1} step_name={"YOUR INFO"} active={true} />;
-  {
-    "#f9818d", "#625aff", "#ffaf7e";
-  }
+
+  // "total form size 600x940";
 }
