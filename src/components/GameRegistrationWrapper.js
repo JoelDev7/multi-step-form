ae5ea1fdc201364619447ad653ee14caf0c0aa4a
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-17 17:30:06 -0600

    Wrapper component created

---
+++ src/components/GameRegistrationWrapper.js
@@ -0,0 +1,9 @@
+import Sidevar from "./sidevar/Sidevar";
+
+export default function GameRegistrationWrapper() {
+  return (
+    <div className="p-5 my-auto">
+      <Sidevar />
+    </div>
+  );
+}
