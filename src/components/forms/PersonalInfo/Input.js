9c75c404e89febe366fde326750ae701f406f8b6
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-18 17:05:34 -0600

    Style added

--- src/components/forms/PersonalInfo/Input.js
+++ src/components/forms/PersonalInfo/Input.js
@@ -1,8 +1,16 @@
 export default function Input({ label, id, type, placeholder, value }) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} placeholder={placeholder} value={value} />
+      <label htmlFor={id} className="text-marine_blue text-sm mb-2">
+        {label}
+      </label>
+      <input
+        id={id}
+        className="border border-light_gray px-4 py-2 font-medium mb-5 rounded-md"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+      />
     </>
   );
 }
