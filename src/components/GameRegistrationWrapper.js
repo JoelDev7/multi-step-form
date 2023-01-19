428399635441643a934a255ac9697609176881dd
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-19 17:19:58 -0600

    Added containers for the inner components to improve the presentation

--- src/components/GameRegistrationWrapper.js
+++ src/components/GameRegistrationWrapper.js
@@ -1,9 +1,16 @@
+import PersonalInfoForm from "./forms/PersonalInfo/PersonalInfoForm";
 import Sidevar from "./sidevar/Sidevar";
 
 export default function GameRegistrationWrapper() {
   return (
-    <div className="p-5 my-auto">
-      <Sidevar />
+    <div className="p-3 my-auto flex md:flex-row bg-white rounded-xl lg:min-h-[500] lg:min-w-[940]">
+      <div>
+        <Sidevar />
+      </div>
+      <div className="w-full flex justify-around ">
+        <PersonalInfoForm />
+      </div>
     </div>
   );
 }
+// "total form size 600x940";
