481acfb6af2a5910814aaf53a2f526a6d9ece7c7
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-19 18:06:14 -0600

    Added an error message display and changed some styles

--- src/components/forms/PersonalInfo/Input.js
+++ src/components/forms/PersonalInfo/Input.js
@@ -1,15 +1,19 @@
-export default function Input({ label, id, type, placeholder, value }) {
+export default function Input({ label, id, type, placeholder, error_message }) {
   return (
     <>
-      <label htmlFor={id} className="text-marine_blue text-sm mb-2">
-        {label}
-      </label>
+      <div className="flex justify-between">
+        <label htmlFor={id} className="text-marine_blue text-sm mb-2">
+          {label}
+        </label>
+        <p className="text-strawberry_red text-sm font-semibold opacity-95">
+          {error_message}
+        </p>
+      </div>
       <input
         id={id}
-        className="border border-light_gray px-4 py-2 font-medium mb-5 rounded-md"
+        className="border border-light_gray text-marine_blue px-4 py-2 font-medium mb-5 rounded-md focus-within:outline-none focus-within:border-purplish_blue"
         type={type}
         placeholder={placeholder}
-        value={value}
       />
     </>
   );
