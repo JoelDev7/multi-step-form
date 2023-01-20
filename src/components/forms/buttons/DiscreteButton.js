e7ffc1584bd643aeb6b64ae1e01a54865e803d37
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-01-19 18:07:43 -0600

    Some styles changed

--- src/components/forms/buttons/DiscreteButton.js
+++ src/components/forms/buttons/DiscreteButton.js
@@ -1,6 +1,6 @@
 export default function DiscreteButton({ text }) {
   return (
-    <button className="text-light_gray font-semibold hover:text-marine_blue  px-5 py-2 rounded-md">
+    <button className="text-cool_gray font-semibold hover:text-marine_blue  px-5 py-2 rounded-md">
       {text}
     </button>
   );
