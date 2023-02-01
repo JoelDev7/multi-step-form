062dd9ece73276f1fbb77aba4bdd6a6845af5a14
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-01 16:48:56 -0600

    Deleted unnecessary click event

--- src/components/forms/buttons/DiscreteButton.js
+++ src/components/forms/buttons/DiscreteButton.js
@@ -1,6 +1,12 @@
-export default function DiscreteButton({ text }) {
+export default function DiscreteButton({
+  text,
+  action = () => console.log(text + " button pressed"),
+}) {
   return (
-    <button className="text-cool_gray font-semibold hover:text-marine_blue  px-5 py-2 rounded-md">
+    <button
+      className="text-cool_gray font-semibold hover:text-marine_blue  px-5 py-2 rounded-md"
+      onClick={() => action()}
+    >
       {text}
     </button>
   );
