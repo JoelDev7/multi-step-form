49e60fe8ee30c59033d9a2efd3a2b4e354698a00
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-01 16:48:07 -0600

    Added the necesary attributes to make possible trigger a submit event from different origins

--- src/components/forms/buttons/SolidButton.js
+++ src/components/forms/buttons/SolidButton.js
@@ -1,6 +1,10 @@
-export default function SolidButton({ text }) {
+export default function SolidButton({ text, form_id }) {
   return (
-    <button className="text-white bg-marine_blue hover:bg-opacity-90 px-5 py-2 rounded-md">
+    <button
+      className="text-white bg-marine_blue hover:bg-opacity-90 px-5 py-2 rounded-md"
+      type="submit"
+      form={form_id}
+    >
       {text}
     </button>
   );
