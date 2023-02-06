3c0d073678ad9096bdcade33f49a4199aa82697d
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-06 12:59:10 -0600

    Changed some of the logic to incluide the form container and added validations to displayActiveForm method so no invalid index positions can be set

--- src/components/GameRegistrationWrapper.js
+++ src/components/GameRegistrationWrapper.js
@@ -3,40 +3,52 @@ import PersonalInfoForm from "./forms/PersonalInfo/PersonalInfoForm";
 import Sidevar from "./sidevar/Sidevar";
 import DiscreteButton from "./forms/buttons/DiscreteButton";
 import SolidButton from "./forms/buttons/SolidButton";
+import ActiveFormContainer from "./forms/ActiveFormContainer";
 
 export default function GameRegistrationWrapper() {
-  const [formData, setFormData] = useState("");
   const [activeForm, setActiveForm] = useState(0);
+  const [formData, setFormData] = useState({});
+  const generalFormProps = {
+    formData: formData,
+    addFormData: addFormData,
+    activeForm: activeForm,
+    displayActiveForm: displayActiveForm,
+  };
   const forms = [
     {
+      title: "Personal Info",
+      form_description:
+        "Please provide your name, email address, and phone number.",
       form: (
-        <PersonalInfoForm
-          formId={"PersonalInfoForm"}
-          formData={formData}
-          addFormData={addFormData}
-          activeForm={activeForm}
-          displayActiveForm={displayActiveForm}
-        />
+        <PersonalInfoForm {...generalFormProps} formId={"PersonalInfoForm"} />
       ),
       formId: "PersonalInfoForm",
     },
-    { form: <h1>Title 2</h1>, formId: "" },
-    { form: <h1>Title 3</h1>, formId: "" },
-    { form: <h1>Title 4</h1>, formId: "" },
+    {
+      title: "Other",
+      form_description:
+        "Please provide your name, email address, and phone number.",
+      form: (
+        <PersonalInfoForm {...generalFormProps} formId={"PersonalInfoForm"} />
+      ),
+      formId: "PersonalInfoForm",
+    },
   ];
-  /**
-   * Sets de active state of the form currently displayed and marks the step associated.
-   * @param {number} index The index associated to the active step and form components.
-   */
-  function displayActiveForm(index) {
-    setActiveForm(index);
-  }
   /**
    * Adds the local data from the form to the general data container to crearte the registration.
    * @param {object} data The data from the current form.
    */
   function addFormData(data) {
     setFormData({ ...formData, ...data });
+  }
+  /*
+   * Sets de active state of the form currently displayed and marks the step associated.
+   * @param {number} index The index associated to the active step and form components.
+   */
+  function displayActiveForm(index) {
+    if (index <= forms.length - 1) {
+      setActiveForm(index);
+    }
   }
   /**
    * Sends the subscription petition to the API.
@@ -53,7 +65,9 @@ export default function GameRegistrationWrapper() {
           displayActiveForm={displayActiveForm}
         />
       </div>
-      <div className="w-full flex justify-around">{forms[activeForm].form}</div>
+      <div className="w-full flex justify-around">
+        <ActiveFormContainer {...generalFormProps} {...forms[activeForm]} />
+      </div>
       <div className="flex md:hidden w-full pl-0 pr-4 py-5 bg-light_gray bg-opacity-60 justify-between items-center">
         {activeForm !== 0 ? <DiscreteButton text={"Go Back"} /> : <div></div>}
         <SolidButton text={"Next Step"} form_id={forms[activeForm].formId} />
