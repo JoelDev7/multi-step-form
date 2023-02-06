9abab4b115bc2cdab22965f6cdd4e3e5d017ea59
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-06 16:51:44 -0600

    Added available plan array and deleted elements that are not used

--- src/components/forms/SelectYourPlan/SelectYourPlanForm.js
+++ src/components/forms/SelectYourPlan/SelectYourPlanForm.js
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
-import DiscreteButton from "../buttons/DiscreteButton";
-import SolidButton from "../buttons/SolidButton";
+import PlanCard from "./PlanCard";
+import arcade from "../../../img/icon-arcade.svg";
+import advanced from "../../../img/icon-advanced.svg";
+import pro from "../../../img/icon-pro.svg";
 
 export default function SelectYourPlanForm(
   formId,
@@ -9,6 +11,23 @@ export default function SelectYourPlanForm(
   activeForm,
   displayActiveForm
 ) {
+  const planCards = [
+    {
+      plan_icon: arcade,
+      plan_title: "Arcade",
+      plan_cost: "$9/mo",
+    },
+    {
+      plan_icon: advanced,
+      plan_title: "Advanced",
+      plan_cost: "$12/mo",
+    },
+    {
+      plan_icon: pro,
+      plan_title: "Pro",
+      plan_cost: "$15/mo",
+    },
+  ];
   const {
     register,
     handleSubmit,
@@ -19,28 +38,11 @@ export default function SelectYourPlanForm(
     addFormData(data);
     displayActiveForm(activeForm + 1);
   };
-  <form
-    onSubmit={handleSubmit(onSubmit)}
-    id={formId}
-    className="m-5 p-5 md:p-0 shadow-xl md:shadow-none flex flex-col justify-between relative -top-14 md:top-0 z-10 bg-white rounded-md"
-  >
-    <div>
-      <label
-        htmlFor={formId}
-        className="text-4xl text-marine_blue font-medium inline-block mb-2 mt-2"
-      >
-        {" "}
-        Personal Info
-      </label>
-      <p className="text-cool_gray mb-7">
-        {" "}
-        You have the option of monthly or yearly billing.
-      </p>
-      <div className="flex flex-col">{}</div>
-    </div>
-    <div className="justify-end self-end hidden md:block">
-      <DiscreteButton text={"Go Back"} />
-      <SolidButton text={"Next Step"} />
-    </div>
-  </form>;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} id={formId}>
+      {planCards.map((plan) => {
+        return <PlanCard {...plan} key={plan.plan_title} />;
+      })}
+    </form>
+  );
 }
