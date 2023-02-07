bef41e3ac3ec8cb108ed8ac74227f6ba94edecf1
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-06 19:44:31 -0600

    Added styles for selected and hover states

--- src/components/forms/SelectYourPlan/PlanCard.js
+++ src/components/forms/SelectYourPlan/PlanCard.js
@@ -2,14 +2,26 @@ export default function PlanCard({
   plan_icon,
   plan_title = "Title",
   plan_cost = "Cost",
+  selected_plan,
+  timespan,
+  setSelection,
 }) {
   return (
-    <div className="border border-cool_gray p-3 pt-4 hover:border-marine_blue">
-      <div>{plan_icon}</div>
-      <div>
+    <button
+      className={
+        (selected_plan === plan_title
+          ? "bg-light_blue bg-opacity-20 border-purplish_blue "
+          : "") +
+        "flex items-center md:block border border-light_gray p-3 pt-4 rounded-md hover:border-purplish_blue"
+      }
+    >
+      <div className="flex justify-center items-center md:items-start md:justify-start">
+        <img src={plan_icon} alt="" className="mb-0 md:mb-6" />
+      </div>
+      <div className="text-left ml-4 md:ml-0">
         <h2 className="text-marine_blue font-medium">{plan_title}</h2>
         <p className="text-cool_gray">{plan_cost}</p>
       </div>
-    </div>
+    </button>
   );
 }
