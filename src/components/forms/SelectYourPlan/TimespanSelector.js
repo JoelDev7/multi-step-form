91f598529a0afd8cc061d83b5a38a9ca4e62e086
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-14 13:57:40 -0600

    Creared a component to select the timespan of the plan active

--- src/components/forms/SelectYourPlan/TimespanSelector.js
+++ src/components/forms/SelectYourPlan/TimespanSelector.js
@@ -1,9 +1,21 @@
-export default function TimespanSelector({ timespan, setSelection }) {
+import { useState } from "react";
+
+export default function TimespanSelector({
+  selected_plan,
+  is_yearly,
+  setSelection,
+}) {
+  const [buttonState, setButtonState] = useState(is_yearly);
+  function changeSelection() {
+    const newState = buttonState ? false : true;
+    setButtonState(newState);
+    setSelection({ selected_plan, is_yearly: newState });
+  }
   return (
     <div className="flex justify-center items-center mt-9 bg-light_gray bg-opacity-30 rounded-md h-10">
       <span
         className={
-          (timespan = "Monthly" ? "text-cool_gray " : "text-marine_blue ") +
+          (buttonState === false ? "text-marine_blue " : "text-cool_gray ") +
           "font-medium"
         }
       >
@@ -11,13 +23,18 @@ export default function TimespanSelector({ timespan, setSelection }) {
       </span>
       <div className="flex">
         <label className="relative inline-flex items-center cursor-pointer mx-4">
-          <input type="checkbox" value="" className="sr-only peer" />
+          <input
+            type="checkbox"
+            checked={buttonState}
+            onChange={changeSelection}
+            className="sr-only peer"
+          />
           <div className="w-9 h-5 bg-marine_blue peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-purplish_blue rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[5px] after:bg-white   after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-marine_blue"></div>
         </label>
       </div>
       <span
         className={
-          (timespan = "Yearly" ? "text-cool_gray " : "text-marine_blue ") +
+          (buttonState === true ? "text-marine_blue " : "text-cool_gray ") +
           "font-medium"
         }
       >
