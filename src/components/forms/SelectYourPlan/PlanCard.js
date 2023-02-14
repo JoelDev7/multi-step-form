4ca6a4453a1e85112b35ebe10cf6fa301b356b9c
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-14 13:56:51 -0600

    Created a component to show the available plans

--- src/components/forms/SelectYourPlan/PlanCard.js
+++ src/components/forms/SelectYourPlan/PlanCard.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function PlanCard({
   plan_icon,
   plan_title = "Title",
   plan_cost = "Cost",
   selected_plan,
-  timespan,
+  is_yearly,
   setSelection,
 }) {
+  useEffect(() => {
+    setActiveDiscount(is_yearly);
+  }, [is_yearly]);
+  const [activeDiscount, setActiveDiscount] = useState();
+  /**
+   * Sets the current plan_title prop as the plan selected.
+   */
+  function setPlan() {
+    setSelection({ selected_plan: plan_title, is_yearly });
+  }
   return (
     <button
       className={
@@ -14,6 +26,8 @@ export default function PlanCard({
           : "") +
         "flex items-center md:block border border-light_gray p-3 pt-4 rounded-md hover:border-purplish_blue"
       }
+      type="button"
+      onClick={setPlan}
     >
       <div className="flex justify-center items-center md:items-start md:justify-start">
         <img src={plan_icon} alt="" className="mb-0 md:mb-6" />
@@ -21,6 +35,9 @@ export default function PlanCard({
       <div className="text-left ml-4 md:ml-0">
         <h2 className="text-marine_blue font-medium">{plan_title}</h2>
         <p className="text-cool_gray">{plan_cost}</p>
+        {activeDiscount ? (
+          <p className="text-sm text-marine_blue">2 months free</p>
+        ) : null}
       </div>
     </button>
   );
